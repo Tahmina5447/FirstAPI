03085a12f6eb3c40658d01bacde640d3f115ca32
Tahmina <tahminakhatun5447@gmail.com>
2023-07-08 17:53:18 +0600

    create gitignore file

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const usersRouter=require("./routes/v1/user.route.js")
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1/user",usersRouter)
-app.use("/api/v1/user/random",usersRouter)
+app.use("api/v1/user",usersRouter)
 app.listen(5000,()=>{
     console.log("server running on port 5000");
 })
